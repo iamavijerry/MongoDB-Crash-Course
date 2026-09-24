29d309a7e73c1f5ee7e05141658f6afcb33464be
iamavijerry <iamavijerry@users.noreply.github.com>
2026-09-24 11:54:27 +0530

    commit

--- timepass.js
+++ timepass.js
@@ -10,8 +10,8 @@ console.log("Date 3", date3);
 
 
 //  shortcut for this
-let oldDate = new Date(new Date().setFullYear( new Date().getFullYear() - 5))
-console.log("5 year old date from now : " , oldDate);
+let oldDate = new Date(new Date().setFullYear(new Date().getFullYear() - 5))
+console.log("5 year old date from now : ", oldDate);
 
 let moneyInPesa = 6999900
 let moneyInRupees = moneyInPesa / 100
