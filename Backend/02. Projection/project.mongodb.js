4fcf2becc26648e581d49165c46ab429a908665e
iamavijerry <iamavijerry@users.noreply.github.com>
2026-09-24 12:17:21 +0530

    aggregation

--- Backend/02. Projection/project.mongodb.js
+++ Backend/02. Projection/project.mongodb.js
@@ -6,5 +6,14 @@ show('collections')
 
 db.users.find({});
 
-db.users.find({}, {email : 1, _id : 0})
-db.users.find({}, {name : 1, email : 1, _id : 0})
+
+//syntax 
+// db.<collection_name>.find({query}, {aggregation})
+
+//field that i want to show
+db.users.find({}, {email : 1, _id : 0});
+db.users.find({}, {name : 1, email : 1, _id : 0});
+
+// field that i dont want to show
+
+db.users.find({}, {age : 0})
